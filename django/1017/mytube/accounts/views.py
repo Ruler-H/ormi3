649b0e489517040f837c9e30bfb7b30d2293d033
byeongheon.hwang <elwl5515@gmail.com>
2023-10-18 21:56:56 +0900

    ADD: Study file

--- django/1017/mytube/accounts/views.py
+++ django/1017/mytube/accounts/views.py
@@ -7,18 +7,18 @@ from django.contrib.auth.decorators import login_required
 signup = CreateView.as_view(
     form_class = UserCreationForm,
     template_name = 'accounts/form.html',
-    success_url = 'tube/',
+    success_url = '/tube/',
 )
 
 login = LoginView.as_view(
     template_name = 'accounts/form.html',
-    next_page = 'tube/',
+    next_page = '/tube/',
 )
 
 logout = LogoutView.as_view(
-    next_page = 'tube/',
+    next_page = '/tube/',
 )
 
 @login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    return render(request, '/accounts/profile.html')
--- django/1017/mytube/tube/views.py
+++ django/1017/mytube/tube/views.py
@@ -1,9 +1,10 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.db.models import Q
-from .models import TubePost, Comment
+from django.urls import reverse_lazy
+from .models import TubePost, Comment, Tag
 from .forms import CommentForm
 
 class TubePostList(ListView):
@@ -23,22 +24,51 @@ class TubePostList(ListView):
 class TubePostDetail(DetailView):
     model = TubePost
 
-    def post(pk, self):
-        tubePost = TubePost.objects.get(pk=self.kwargs['pk'])
-        form = CommentForm(self.request.POST)
-        if form.is_valid():
-            c = Comment.objects.create(
-                post=tubePost,
-                message=form.cleaned_data['message'],
-                author=self.request.user,
-            )
-            c.save()
-
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
         return context
+    
+class TubePostCreate(CreateView):
+    model = TubePost
+    fields = '__all__'
+    success_url = reverse_lazy('tube_list')
+
+class TubePostUpdate(UpdateView):
+    model = TubePost
+    fields = '__all__'
+    success_url = reverse_lazy('tube_list')
+
+class TubePostDelete(DeleteView):
+    model = TubePost
+    success_url = reverse_lazy('tube_list')
+
+def tube_tag(request, tag):
+    tubePosts = TubePost.objects.all()
+    tagTube = []
+    for tubePost in tubePosts:
+        tags = tubePost.tags.filter(Q(name__icontains=tag))
+        if tags:
+            tagTube.append(tubePost)
+    return render(request, 'tube/tubepost_list.html', {'object_list':tagTube})
+
+def comment(request, pk):
+    tubePost = TubePost.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            c = Comment.objects.create(
+                post = tubePost,
+                message = form.cleaned_data['message'],
+                author = request.user,
+            )
+            c.save()
+    new_form = CommentForm()
+    return render(request, 'tube/tubepost_detail.html', {'object':tubePost, 'form':new_form})
         
 
 tube_list = TubePostList.as_view()
 tube_detail = TubePostDetail.as_view()
+tube_create = TubePostCreate.as_view()
+tube_update = TubePostUpdate.as_view()
+tube_delete = TubePostDelete.as_view()
